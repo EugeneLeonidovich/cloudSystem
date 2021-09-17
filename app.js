f385d3db7bc0b0e61968104439e8e9de5579a33c
EugeneLeonidovich <7k6p1wr@gmail.com>
2021-09-17 11:24:43 +0300

    init

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("test");
+  
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
